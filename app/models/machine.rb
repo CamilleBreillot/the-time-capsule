27c624dec5d61d05bed483afcfa3b0de7d8a5f08
camille breillot <camille.breillot@gmail.com>
2021-11-15 16:52:12 +0100

    created machine model and controller (index, show, new, create)

---
+++ app/models/machine.rb
@@ -0,0 +1,3 @@
+class Machine < ApplicationRecord
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_141153) do
+ActiveRecord::Schema.define(version: 2021_11_15_143700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "machines", force: :cascade do |t|
+    t.string "name"
+    t.string "details"
+    t.string "country"
+    t.integer "price"
+    t.integer "period_century"
+    t.integer "period_specific"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_machines_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_11_15_141153) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "machines", "users"
 end
