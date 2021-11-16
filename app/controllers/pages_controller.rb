13adeb6fa5d3b5112acf2d3f5314822984747506
Margot Liard <margliard@gmail.com>
2021-11-16 15:53:43 +0100

    dashboard view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
 
   def dashboard
     @user = current_user
-    @bookings = bookings.where(user: current_user)
-    @machines = machines..where(user: current_user)
+    @bookings = booking.where(user: current_user)
+    @machines = machine..where(user: current_user)
   end
 end
