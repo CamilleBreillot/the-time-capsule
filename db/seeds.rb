8dd6973990e05ab925a561a0ffe49e5c52cb9b08
Aymeric Payen <aymeric.payen@gmail.com>
2021-11-16 18:17:22 +0100

    seeds for booking performed

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,6 +46,9 @@ war.photos.attach(io: file11, filename: 'file11.jpeg', content_type: 'image/jpeg
 file12 = URI.open("https://www.thoughtco.com/thmb/8ECQKF4hNFwia-U3uA8TXBPsZbk=/4151x2335/smart/filters:no_upscale()/new-yorkers-celebrate-v-e-day-514877528-5b93085c46e0fb0025701c7d.jpg")
 war.photos.attach(io: file12, filename: 'file12.jpeg', content_type: 'image/jpeg')
 
+booking1 = Booking.create!(departure_date: "15/12/2021", arrival_date: "31/12/2021", booking_price: 18_000,
+                          booking_confirmed: true, user_id: margot.id, machine_id: war.id)
+
 puts "Finished!"
 
 # require 'faker'
