f96d20f4da35a73b5432c3f6255e9bebb3a6a6d2
camille breillot <camille.breillot@gmail.com>
2021-11-17 14:59:53 +0100

    validation commented out

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,8 +2,7 @@ class Machine < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
   has_many :bookings
+  # validates :name, :details, :place, :price, :period_century, :period_specific, :photos, presence: true
   geocoded_by :place
   after_validation :geocode, if: :will_save_change_to_place?
-  validates :name, uniqueness: true, presence: true
-  validates :details, :place, :price, :period_century, :period_specific, :photos, presence: true
 end
