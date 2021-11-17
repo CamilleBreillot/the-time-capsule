5dde5043e7dfed95651a964c9b4d8154366cd11d
sophie callault <s.callault@gmail.com>
2021-11-17 14:32:19 +0100

    mapbox prnted

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
-import mapboxgl from 'mapbox-gl';
+import mapboxgl from '!mapbox-gl';
 
 export default class extends Controller {
   static values = {
@@ -9,12 +9,13 @@ export default class extends Controller {
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue;
-    this._addMarkersToMap();
 
     this.map = new mapboxgl.Map({
       container: this.element,
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+    this._addMarkersToMap();
+    this._fitMapToMarkers();
   }
 
   _addMarkersToMap() {
@@ -23,5 +24,10 @@ export default class extends Controller {
         .setLngLat([marker.lng, marker.lat])
         .addTo(this.map);
     });
+  }
+  _fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 }
