b0e1650e3b6509649e852577ee13ab87735bbe97
camille breillot <camille.breillot@gmail.com>
2021-11-17 14:07:31 +0100

    addingvalidationtomachines

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,4 +2,6 @@ class Machine < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
   has_many :bookings
+  validates :name, uniqueness: true, presence: true
+  validates :details, :country, :price, :period_century, :period_specific, :photos, presence: true
 end
